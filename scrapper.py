f08e4d5df0af1d52cbca8f440fd05568f7da4019
AlexandreLadriere <alexandre.ladriere77@gmail.com>
2022-04-05 20:07:29 +0200

    add: get object thumbnail(s)

--- scrapper.py
+++ scrapper.py
@@ -3,7 +3,8 @@ import requests
 SPECIAL_CHAR = ['\n', '\t']
 COLUMNS_FILE = 'columns.txt'
 MUSEUM_URL = 'http://www.muis.ee/rdf/objects-by-museum/83529'
-OBJECT_URL = 'http://opendata.muis.ee/object/1858783'
+#OBJECT_URL = 'http://opendata.muis.ee/object/1858783'
+OBJECT_URL = 'http://opendata.muis.ee/object/1259094'
 
 
 def remove_character(char_list, str):
@@ -40,14 +41,28 @@ def get_object_info(object_page, info):
                     i += 1
             object_info.append(remove_character(SPECIAL_CHAR, value))
     return object_info
-                
+
+# get thumbnail(s) of the object 
+def get_thumbnail_url(object_page):
+    thumbnail_balise = "id=\"thumbnail_"
+    thumbnail_url = ''
+    split_object_page_text = object_page.text.split(thumbnail_balise)
+    if len(split_object_page_text) == 1: # no thumbnail found
+        return thumbnail_url
+    else:
+        for i in range(1, len(split_object_page_text)):
+            thumbnail_url += (split_object_page_text[i].split("\"", 3))[2] + '\n'
+    return thumbnail_url
 
 
 columns = get_columns(COLUMNS_FILE)
 object_page = requests.get(OBJECT_URL)
 object_info = get_object_info(object_page, columns)
+object_info[1] = OBJECT_URL
+object_info[2] = get_thumbnail_url(object_page)
 print(object_info)
 
 
 # chercher thumbnail url
 # cas spécial pour dimensions
+# cas pour plusieurs dimensions, plusieurs images, eetc (object composé de plusieurs éléments)
